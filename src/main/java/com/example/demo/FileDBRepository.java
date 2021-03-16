c22d1d17b7b709957b6fbed081a49657ef86431a
Gayath1 <chandulagayan@gmail.com>
2021-03-16 09:57:10 +0530

    changes

--- src/main/java/com/example/demo/FileDBRepository.java
+++ src/main/java/com/example/demo/FileDBRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo;
 
-public class FileDBRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+
 }
