0d525abee6e9284723e4b016992477b4a33823ca
Gayath1 <chandulagayan@gmail.com>
2021-03-25 19:30:30 +0530

    route changes

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
 
                 .antMatchers("/").access("hasAuthority('User') or hasAuthority('Admin')")
-                .antMatchers("/reasearch","/update","/delete","/file","user/research").access("hasAuthority('Researcher') or hasAuthority('Admin')")
+                .antMatchers("/reasearch","/update","/delete","user/research").access("hasAuthority('Researcher') or hasAuthority('Admin')")
                 .antMatchers("/retrive").access("hasAuthority('Funder') or hasAuthority('Admin')")
                 .antMatchers("/admin").access("hasAuthority('Admin')")
                 .anyRequest().authenticated()
