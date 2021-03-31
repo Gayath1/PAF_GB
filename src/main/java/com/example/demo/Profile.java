b93c5f7134ff3376967a20c1ef635c71b3fdb0d1
Gayath1 <chandulagayan@gmail.com>
2021-03-31 09:04:04 +0530

    user profile

--- src/main/java/com/example/demo/Profile.java
+++ src/main/java/com/example/demo/Profile.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,6 +13,8 @@ public class Profile {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String sayPlainTextHello() {
-        return "Hello Jersey";
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        String email = loggedInUser.getName();
+        return "Hello" + email;
     }
 }
