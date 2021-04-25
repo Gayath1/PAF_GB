e99779831b2bbde54e820952fb31ece4aeb4a7de
Gayath1 <chandulagayan@gmail.com>
2021-04-25 19:32:27 +0530

    changes of path

--- src/main/java/com/example/demo/FundingService.java
+++ src/main/java/com/example/demo/FundingService.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 //For JSON
-
+//import com.google.gson.*;
 //For XML
 import org.jsoup.*;
 import org.jsoup.parser.*;
@@ -40,15 +40,20 @@ public class FundingService {
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String updateFunding(String fundingData) {
+	public String updateFunding(@FormParam("fid")
+											String fid, @FormParam("ftype") String ftype,
+
+								@FormParam("fsource") String fsource,
+								@FormParam("famount") String famount,
+								@FormParam("fdate") String fdate) {
 		// Convert the input string to a JSON object
-		JsonObject fundingObject = new JsonParser().parse(fundingData).getAsJsonObject();
+		//JsonObject fundingObject = new JsonParser().parse(fundingData).getAsJsonObject();
 		// Read the values from the JSON object
-		String fid = fundingObject.get("fid").getAsString();
-		String ftype = fundingObject.get("ftype").getAsString();
-		String fsource = fundingObject.get("fsource").getAsString();
-		String famount = fundingObject.get("famount").getAsString();
-		String fdate = fundingObject.get("fdate").getAsString();
+		//String fid = fundingObject.get("fid").getAsString();
+		//String ftype = fundingObject.get("ftype").getAsString();
+		//String fsource = fundingObject.get("fsource").getAsString();
+		//String famount = fundingObject.get("famount").getAsString();
+		//String fdate = fundingObject.get("fdate").getAsString();
 
 		String output = FundingObj.updateFunding(fid, ftype, fsource, famount, fdate);
 		return output;
@@ -58,12 +63,13 @@ public class FundingService {
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String deleteFunding(String fundingData) {
+	public String deleteFunding(@FormParam("fid")
+											String fid) {
 		// Convert the input string to an XML document
-		Document doc = Jsoup.parse(fundingData, "", Parser.xmlParser());
+		//Document doc = Jsoup.parse(fundingData, "", Parser.xmlParser());
 
 		// Read the value from the element <itemID>
-		String fid = doc.select("fid").text();
+		//String fid = doc.select("fid").text();
 		String output = FundingObj.deleteFunding(fid);
 		return output;
 	}
